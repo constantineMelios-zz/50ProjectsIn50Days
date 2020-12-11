d473995d7637e26e05197d3eb1d1266fa71b218d
Constantine Melios <constantinemelios@gmail.com>
2020-12-11 07:56:19 +0200

    Day 8 - Wave Animation Form

---
+++ Day 8/script.js
@@ -0,0 +1,7 @@
+const labels = document.querySelectorAll('label')
+
+labels.forEach(label => {
+  label.innerHTML = label.innerText.split('').map((letter, index) => {
+    return `<span style="transition-delay:${index * 40}ms">${letter}</span>`
+  }).join('')
+})
